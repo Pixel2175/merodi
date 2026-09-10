1c46a05ad18e2ecf4681ce40f1d52ec369abbe68
pi66 <pixel2176@proton.me>
2026-09-10 21:26:42 +0100

    docs: update plugin examples for enable/disable
    Replace extensions.lua with enable.lua and disable.lua examples
    showing the new callable API: merodi.enable.X() and
    merodi.disable.X() instead of bare field access.

---
+++ examples/disable.lua
@@ -0,0 +1,12 @@
+-- HTML attribute filters
+merodi.disable.GlobalAttribute()       -- disables the global attribute filter
+merodi.disable.HeadingAttribute()      -- allows heading attributes
+merodi.disable.BlockquoteAttribute()   -- allows blockquote attributes
+merodi.disable.CodeAttribute()         -- allows code attributes
+merodi.disable.EmphasisAttribute()     -- allows emphasis attributes
+merodi.disable.ImageAttribute()        -- allows image attributes
+merodi.disable.LinkAttribute()         -- allows link attributes
+merodi.disable.ListAttribute()         -- allows list attributes
+merodi.disable.ListItemAttribute()     -- allows list-item attributes
+merodi.disable.ParagraphAttribute()    -- allows paragraph attributes
+merodi.disable.ThematicAttribute()     -- allows thematic-break attributes
---
+++ examples/enable.lua
@@ -0,0 +1,27 @@
+merodi.enable.CJK()              -- handles Chinese, Japanese, and Korean text
+merodi.enable.DefinitionList()   -- supports definition lists
+merodi.enable.Footnote()         -- supports footnotes
+merodi.enable.GFM()              -- enables GitHub-Flavored Markdown
+merodi.enable.Linkify()          -- turns URLs and emails into links
+merodi.enable.Strikethrough()    -- supports ~~strikethrough~~
+merodi.enable.Table()            -- supports Markdown tables
+merodi.enable.TaskList()         -- supports task lists
+merodi.enable.Typographer()      -- replaces quotes and dashes with typographic ones
+merodi.enable.HtmlAttr()         -- allows attributes on Markdown elements
+merodi.enable.AutoHeadingID()    -- adds IDs to headings automatically
+merodi.enable.EscapedSpace()     -- allows spaces to be escaped
+merodi.enable.SetextHeading()    -- supports `Heading\n=======` headings
+merodi.enable.ThematicBreak()    -- supports `---`, `***`, and `___`
+merodi.enable.List()             -- supports ordered and unordered lists
+merodi.enable.ListItem()         -- parses list items
+merodi.enable.CodeBlock()        -- supports indented code blocks
+merodi.enable.ATXHeading()       -- supports `# Heading` headings
+merodi.enable.FencedCodeBlock()  -- supports fenced code blocks
+merodi.enable.Blockquote()       -- supports `> quotes`
+merodi.enable.HTMLBlock()        -- supports block-level HTML
+merodi.enable.Paragraph()        -- parses normal paragraphs
+merodi.enable.CodeSpan()         -- supports `` `code` ``
+merodi.enable.Link()             -- supports `[text](url)` links
+merodi.enable.AutoLink()         -- supports `<https://example.com>` links
+merodi.enable.RawHTML()          -- supports inline HTML
+merodi.enable.Emphasis()         -- supports *italic* and **bold**
